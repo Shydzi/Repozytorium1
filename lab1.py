08f262ea4adb8e27bf4ef05346d60aede3175ad9
Shydzi <97967903+Shydzi@users.noreply.github.com>
2022-03-16 12:59:51 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,51 @@
+import math
+# #----zadanie 1----#
+#
+#
+# a=2
+# b=2.7
+#
+# print(type(a),type(b))
+# #----zadanie 2----#
+# a2= 2
+# b2= 3
+# print("podaj znak")
+# znak=input()
+# if(znak=='/'):
+#     print('dzielenie liczb {0:d} przez {1:d} Wynosi {2:f}'.format(a2,b2,a2/b2))
+# elif znak=='+':
+#     print('dodawanie liczb {0:d} i {1:d} Wynosi {2:d}'.format(a2,b2,a2+b2))
+# elif znak=='-':
+#     print('odejmowanie liczb {0:d} i {1:d} Wynosi {2:d}'.format(a2,b2,a2-b2))
+# elif znak=='*':
+#     print('mnozenie liczb {0:d} i {1:d} Wynosi {2:d}'.format(a2,b2,a2*b2))
+# else:
+#     print('zły znak')
+# #----zadanie 3----#
+# a3=2
+# b3=5
+# a3 += b3
+# print(a3)
+# a3=2
+# b3=5
+# a3-=b3
+# print(a3)
+# a3=2
+# b3=5
+# a3*=b3
+# print(a3)
+# a3=2
+# b3=5
+# a3/=b3
+# print(a3)
+# a3=2
+# b3=5
+# a3%=b3
+# print(a3)
+# a3=2
+# b3=5
+# a3**=b3
+# print(a3)
+#----zadanie 4----#
+print(math.pow(math.e,10))
+print(math.log(5+))
