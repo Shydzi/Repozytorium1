cf53f8d61698fd0ad0e9bcf367b52be4904473a3
Shydzi <97967903+Shydzi@users.noreply.github.com>
2022-04-13 12:38:37 +0200

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,64 @@
+import numpy as np
+#====zadanie 1====#
+# a = np.array((1,2,3))
+# b = np.array((4,5,6))
+# c = a*b
+# print(c)
+#====zadanie 2====#
+# a = np.arange(9).reshape((3,3))
+# b = np.arange(16).reshape((4,4))
+# print(np.amin(a, axis=1))
+# print(np.amin(a, axis=0))
+# print(np.amin(b, axis=1))
+# print(np.amin(b, axis=0))
+# for x in a[]
+#====zadanie 3====#
+# a = np.array((1,2,3))
+# b = np.array((4,5,6))
+# c = a.dot(b)
+# print(c)
+#====zadanie 4====#
+# a = np.array((1.2,2.4,3.5))
+# b = np.array((4,5,6))
+# c = a * b
+# print(c)
+#====zadanie 5====#
+# a = np.arange(6).reshape(2,3)
+# print(a)
+# a = np.sin(a)
+# print(a)
+#====zadanie 6====#
+# b = np.arange(6).reshape(2,3)
+# print(b)
+# b = np.cos(b)
+# print(b)
+#====zadanie 7====#
+# c = a + b
+# print(c)
+#====zadanie 8====#
+a = np.arange(9).reshape(3,3)
+for x in a:
+    print(x)
+#====zadanie 9====#
+# a = np.arange(9).reshape(3,3)
+# for x in a.flat:
+#     print(x)
+#====zadanie 10====#
+# a = np.arange(81).reshape(9,9)
+# b = np.reshape(a, (3,27))
+# c = np.reshape(a, (27,3))
+# print(a)
+# print(b)
+# print(c)
+#====zadanie 11====#
+# a = np.arange(12)
+# print(a)
+# a = np.reshape(a, (3,4))
+# # print(a)
+# b = np.arange(12).reshape(4,3)
+# # print(b)
+# print(np.ravel(b))
+# c = np.arange(12).reshape(2,6)
+# # print(c)
+# print(np.ravel(c))
+
