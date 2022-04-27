c99cce0965bbc3d4a230fdd30e04f7ed9458c1e9
Shydzi <97967903+Shydzi@users.noreply.github.com>
2022-04-27 10:53:12 +0200

    Update cw8.py

--- cw8.py
+++ cw8.py
@@ -3,19 +3,23 @@ import pandas as pd
 #====zadanie 2====#
 df = pd.read_csv('imiona.csv',header=0, sep=',', decimal='.')
 # print(df)
+#====zadanie 2.1====#
 # print(df[df['Liczba'] > 999])
+#====zadanie 2.2====#
 # print(df.loc[df['Imie']=='PAWEŁ'])
+#====zadanie 2.3====#
 # print(df[((df.Rok > 2009) & (df.Rok< 2012))])
+#====zadanie 2.4====#
 # m = df.loc[df['Plec']=='M']
 # k = df.loc[df['Plec']=='K']
 # mm = m[['Liczba']]
 # mk = k[['Liczba']]
 # print(f'Suma dziewcząt:{mk.sum()}\n Suma chłopców:{mm.sum()}\n')
-
+#====zadanie 2.5====#
 # m = df.loc[df['Rok']<=2005]
 # mk = m[['Liczba']]
 # print(mk.sum())
-#
+##====zadanie 2.6====#
 # liczba = df[['Liczba']]
 # print(liczba.sum())
 
@@ -27,20 +31,20 @@ df = pd.read_csv('imiona.csv',header=0, sep=',', decimal='.')
 #====zadanie 3====#
 df = pd.read_csv('zamowienia.csv', header=0, sep=';', decimal='.')
 print(df)
-
+#====zadanie 3.1====#
 # m = np.unique(df[['Sprzedawca']])
 # m = df.drop_duplicates(['Sprzedawca'])[['Sprzedawca']]
 # print(m)
-
+#====zadanie 3.2====#
 # m = df['Utarg'].nlargest(n=5)
 # print(m)
-
+#====zadanie 3.3====#
 # m = df.groupby(['Sprzedawca']).count()
 # print(m)
-
+#====zadanie 3.4====#
 # m = df.groupby(['Kraj']).count()
 # print(m)
-
+#====zadanie 3.5====#
 # filter1 = (df['Data zamowienia'] > '2005-01-01') & (df['Data zamowienia'] < '2006-01-01')
 # filter2 = df['Kraj']=='Polska'
 #
@@ -48,7 +52,7 @@ print(df)
 
 # filter1 = (df['Data zamowienia'] > '2004-01-01') & (df['Data zamowienia'] < '2005-01-01')
 # print(df.loc[filter1]['Utarg'].mean())
-
+#====zadanie 3.6====#
 filter1 = (df['Data zamowienia'] >= '2004-01-01') & (df['Data zamowienia'] < '2005-01-01')
 print(df.loc[filter1])
 
